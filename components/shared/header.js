fc77353970f4fd84a0e75f0714657a6089aa2637
Iurii Donev <diur@ciklum.com>
2019-08-05 15:23:53 +0300

    5.08.2019

--- components/shared/header.js
+++ components/shared/header.js
@@ -1,28 +1,113 @@
-import { Fragment } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { Link as NextLink } from '../../routes.js';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const Index = () => {
-  return (
-    <Fragment>
-      <Link href='/'>
-        <span>Home</span>
-      </Link>
-      <Link href='/about'>
-        <span>About</span>
-      </Link>
-      <Link href='/portfolios'>
-        <span>Portfolios</span>
-      </Link>
-      <Link href='/cv'>
-        <span>CV</span>
-      </Link>
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
 
-      {/* <NextLink route='test' params={{id: '2'}}><a>Test 2</a></NextLink>
-      <NextLink route='/test/5'><a>Test 5</a></NextLink> */}
-
-    </Fragment>
-  );
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar className='port-navbar port-default absolute' color="transparent" dark expand="md">
+          <NavbarBrand className='port-navbar-brand' href="/">Yurii Donev</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem className='port-navbar-item'>
+                <Link href='/'>
+                  <a className='nav-link port-navbar-link'>Home</a>
+                </Link>
+              </NavItem>
+              <NavItem className='port-navbar-item'>
+                <Link href='/about'>
+                  <a className='nav-link port-navbar-link'>About</a>
+                </Link>
+              </NavItem>
+              <NavItem className='port-navbar-item'>
+                <Link href='/portfolios'>
+                  <a className='nav-link port-navbar-link'>Home</a>
+                </Link>
+              </NavItem>
+              <NavItem className='port-navbar-item'>
+                <Link href='/cv'>
+                  <a className='nav-link port-navbar-link'>CV</a>
+                </Link>
+              </NavItem>
+              {/* <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown> */}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default Index;
+
+
+// import { Fragment } from 'react';
+// import Link from 'next/link';
+// import { Link as NextLink } from '../../routes.js';
+//
+// const Index = () => {
+//   return (
+//     <Fragment>
+//       <Link href='/'>
+//         <span>Home</span>
+//       </Link>
+//       <Link href='/about'>
+//         <span>About</span>
+//       </Link>
+//       <Link href='/portfolios'>
+//         <span>Portfolios</span>
+//       </Link>
+//       <Link href='/cv'>
+//         <span>CV</span>
+//       </Link>
+//
+//       {/* <NextLink route='test' params={{id: '2'}}><a>Test 2</a></NextLink>
+//       <NextLink route='/test/5'><a>Test 5</a></NextLink> */}
+//
+//     </Fragment>
+//   );
+// }
+//
+// export default Index;
--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ class Index extends Component {
               <Col md="6" className="hero-welcome-wrapper">
                 <div className="hero-welcome-text">
                   <h1>
-                    Welcome to the portfolio website of Filip Jerga.
+                    Welcome to the portfolio website of Yurii Donev.
                     Get informed, collaborate and discover projects I was working on through the years!
                   </h1>
                 </div>
