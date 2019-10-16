ece24ab3013b7c92844edff0f5a4f0a63408e414
Iurii Donev <diur@ciklum.com>
2019-10-16 11:57:27 +0300

    bug fixed

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,8 +1,8 @@
 // Add here value before deploy
-// module.export = {
-//   DB_URI: process.env.DB_URI
-// }
-
-module.export = {
-  DB_URI: 'mongodb+srv://donev:donev@cluster0-injzx.mongodb.net/test?retryWrites=true&w=majority'
+module.exports = {
+  DB_URI: process.env.DB_URI
 }
+
+// module.exports = {
+//   DB_URI: 'mongodb+srv://donev:donev@cluster0-injzx.mongodb.net/test?retryWrites=true&w=majority'
+// }
