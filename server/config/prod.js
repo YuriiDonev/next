8e41d11bcfaf74630824ab2def96422305b93e4c
Iurii Donev <diur@ciklum.com>
2019-10-16 11:46:18 +0300

    update db uri

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,8 +1,8 @@
 // Add here value before deploy
-module.export = {
-  DB_URI: process.env.DB_URI
-}
-
 // module.export = {
-//   DB_URI: 'mongodb+srv://donev:donev@cluster0-injzx.mongodb.net/test?retryWrites=true&w=majority'
+//   DB_URI: process.env.DB_URI
 // }
+
+module.export = {
+  DB_URI: 'mongodb+srv://donev:donev@cluster0-injzx.mongodb.net/test?retryWrites=true&w=majority'
+}
